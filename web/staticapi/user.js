8ddf7a76af03463f3a7d0560e35d30afec06c99f
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-10-10 23:46:16 +0100

    user class using faux login func

---
+++ web/staticapi/user.js
@@ -0,0 +1,35 @@
+import * as db from "./login";
+
+export class User {
+	/** @property {Number} id Unique User ID */
+	id
+	/** @property {String} email Email Address */
+	email
+	/** @property {Number} rank User rank in the company */
+	rank
+
+	/**
+	 * @param {Number} id Unique User ID
+	 * @param {String} email Email Address
+	 * @param {Number} rank User rank in the company
+	 */
+	constructor(id, email, rank) {
+		this.id = id;
+		this.email = email;
+		this.rank = rank;
+	}
+
+	/**
+	 * Obtains the user if the email & password are in the database
+	 * @param {String} email User email
+	 * @param {String} password User password
+	 * @returns {User} Returns the User or null
+	 */
+	static login(email, password) {
+		let res = db.login(email, password);
+		if (res.status == "success")
+			return new User(res.user.id, res.user.email, res.user.type);
+		return null;
+	}
+
+}
