265af019396c543fbc6032479e42bfaae64d7689
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-10-12 20:11:31 +0100

    A user has a personal list of tasks and a tasklist function to find all their assigned tasks

--- web/api/user.js
+++ web/api/user.js
@@ -1,3 +1,7 @@
+import { Task, TaskSrc } from "./task";
+
+/** @typedef {import('./task.js').TaskRef} TaskRef */
+
 export class User {
   /** @property {Number} id Unique User ID */
   id;
@@ -7,17 +11,33 @@ export class User {
   rank;
   /** @property {String} name The users full name */
   name;
+  /** @property {Array<Task>} tasks*/
+  tasks;
 
   /**
    * @param {Number} id Unique User ID
    * @param {String} email Email Address
    * @param {Number} rank User rank in the company
    * @param {String} name The users full name
+   * @param {Array<Task>} [tasks] A list of tasks
    */
-  constructor(id, email, rank, name) {
+  constructor(id, email, rank, name, tasks = []) {
     this.id = id;
     this.email = email;
     this.rank = rank;
     this.name = name;
+    this.tasks = tasks;
+  }
+
+  /**
+   * Returns a list fo all tasks that this user has been assigned too, and the location they were assigned from.
+   * @returns {Array<TaskRef>} TaskRef objects contain a task, and the source that the task came from.
+   */
+  tasklist() {
+    return [
+      ...this.tasks.map((task) => {
+        return { task: task, source: TaskSrc.User };
+      }),
+    ];
   }
 }
