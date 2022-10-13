6c6860ec38cf684d849dbabdd1fadda4ab4e25f9
Adam Hutton <A.Hutton-21@student.lboro.ac.uk>
2022-10-13 11:17:35 +0100

    modal

--- web/dashboard/script.js
+++ web/dashboard/script.js
@@ -1,14 +1,33 @@
 import { Task } from "../api/task.js";
 
-const listParent = document.querySelector("#task-list-overall");
-for (let i = 0; i < 8; i++) {
-  let task = new Task(i, false, null);
-  task.name = `Task ${i}`;
-  listParent.innerHTML += createTaskListItem(task);
+populateTasksList();
+
+const newTaskDialog = document.querySelector("#dialog-new-task");
+
+newTaskDialog.showModal();
+
+const newItemButton = document.querySelector(".new-item");
+newItemButton.addEventListener("click", () => {
+  const newTaskDialog = document.querySelector("#dialog-new-task");
+
+  newTaskDialog.showModal();
+});
+
+/**
+ * Fill out the task list with tasks from the 'database'
+ * @param {number} noItems Number of tasks to display
+ */
+function populateTasksList(noItems = 10) {
+  const listParent = document.querySelector("#task-list-overall");
+  for (let i = 0; i < noItems; i++) {
+    let task = new Task(i, false, null);
+    task.name = `Task ${i}`;
+    listParent.innerHTML += createTaskListItem(task);
+  }
 }
 
 /**
- *
+ * Create checkbox/label/hr elements for a task
  * @param {Task} task
  * @returns {string} HTML for task list item
  */
