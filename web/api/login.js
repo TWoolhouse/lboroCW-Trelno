b052f2cd7d14731b10bfc82277810811c725c559
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-10-15 18:23:57 +0100

    hacky hacky login func

--- web/api/login.js
+++ web/api/login.js
@@ -1,21 +1,25 @@
-import { User } from "./user.js";
+import { User } from "../api/user.js";
 
 /**
  * Fakes a DB call and returns temp example objects
  * @param {string} email
  * @param {string} password
- * @returns success and user object if valid credentials, else error
+ * @returns success and userid number if valid credentials, else error
  */
-export function login(email, password) {
-  if (email == "king@make-it-all.co.uk" && password == "password") {
+export async function login(email, password) {
+  let response = (() => {
+    if (email == "king@make-it-all.co.uk" && password == "password") {
+      return 1;
+    } else if (email == "serf@make-it-all.co.uk" && password == "pswd") {
+      return 1;
+    }
+    return null;
+  })();
+  if (response) {
+    localStorage.setItem("userid", response);
     return {
       status: "success",
-      user: new User(1, "king@make-it-all.co.uk", 1),
-    };
-  } else if (email == "serf@make-it-all.co.uk" && password == "pswd") {
-    return {
-      status: "success",
-      user: new User(2, "serf@make-it-all.co.uk", 6),
+      user: response,
     };
   } else {
     return {
