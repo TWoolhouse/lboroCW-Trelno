da07e63a3f6887ace8420f75df3c722a6236770b
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-10-16 23:40:27 +0100

    new faux id generator and user create func

--- web/api/core.js
+++ web/api/core.js
@@ -40,13 +40,40 @@ export async function team(id) {
  * @returns {Task}
  */
 export async function task_create(done, name) {
+  let t = new Task(id_gen(task), done, name);
+  return await Memoize.Type(Task.name).set(t);
+}
+
+/**
+ * Creates a new user
+ * @param {String} email
+ * @param {UserType} rank
+ * @param {String} name
+ * @returns {User}
+ */
+export async function create_user(email, rank, name) {
+  let obj = new User(id_gen(user), email);
+  return await Memoize.Type(User).set(obj);
+}
+
+// HELPER FAUX GENERATOR
+// //TODO: Move this to faux / memoize
+
+async function id_gen(func) {
   let id = 0;
   do {
     id = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-  } while ((await task(id)) != undefined);
-  let t = new Task(id, done, name);
-  return await Memoize.Type(Task.name).set(t);
+  } while ((await func(id)) != undefined);
+  return id;
 }
+
+// Set the USERID Cookie and local storage // Get it when loaded
+// let userid =
+//   localStorage.getItem("userid") ??
+//   document.cookie
+//     .split("; ")
+//     .find((row) => row.startsWith("userid="))
+//     ?.split("=")[1];
 
 import { memoized } from "./interface/memoize.js";
 console.log(memoized);
