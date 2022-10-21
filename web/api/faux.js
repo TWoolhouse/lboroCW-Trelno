2d2f65ed173ab993f0b4bfbdfc3501afacb6d50f
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-10-21 10:12:43 +0100

    extra faux needs to be randomised

--- web/api/faux.js
+++ web/api/faux.js
@@ -9,25 +9,29 @@ await (async () => {
   // TODO: Temporary Faux database saved in session storage
   if (sessionStorage.getItem("FAUX") != null) return;
   sessionStorage.setItem("FAUX", "1");
-  let t1 = await Memoize.Type(Task).set(new Task(1, TaskState.Ready, "Task 1"));
-  let t2 = await Memoize.Type(Task).set(
+  let t1 = await Memoize.Type(Task).create(
+    new Task(1, TaskState.Ready, "Task 1")
+  );
+  let t2 = await Memoize.Type(Task).create(
     new Task(2, TaskState.Active, "Task 2")
   );
-  let t3 = await Memoize.Type(Task).set(new Task(3, TaskState.Done, "Task 3"));
+  let t3 = await Memoize.Type(Task).create(
+    new Task(3, TaskState.Done, "Task 3")
+  );
 
-  let u1 = await Memoize.Type(User).set(
+  let u1 = await Memoize.Type(User).create(
     new User(1, "king@makeitall.co.uk", UserRank.ProjectManager, "King Firat")
   );
   u1.tasks.add(t2);
-  let u2 = await Memoize.Type(User).set(
+  let u2 = await Memoize.Type(User).create(
     new User(2, "serf@makeitall.co.uk", UserRank.Employee, "Serf Boi")
   );
 
-  await Memoize.Type(Team).set(new Team(1, u1, "My Cool Team"));
+  await Memoize.Type(Team).create(new Team(1, u1, "My Cool Team"));
 
-  let a1 = await Memoize.Type(Assignees).set(new Assignees(1));
+  let a1 = await Memoize.Type(Assignees).create(new Assignees(1));
 
-  await Memoize.Type(Project).set(
+  await Memoize.Type(Project).create(
     new Project(1, u1, new Date(2022, 9, 3), Date.now(), "Best Project", a1)
   );
 })();
