e9102d89f418191e4460d70251451f9c9662e023
TWoolhouse <thomasrwoolhouse@gmail.com>
2022-10-27 11:58:35 +0100

    sandwich class & a cool new colour

--- web/knowledge/post/edit/script.js
+++ web/knowledge/post/edit/script.js
@@ -1,18 +1,16 @@
-console.log(showdown);
-
 const DomIn = document.querySelector(".card.post textarea");
 const DomOut = document.querySelector("#test");
 
 const converter = new showdown.Converter({ noHeaderId: true });
 
 function convert() {
-  const md = DomIn.value;
+  const md = DomIn.value ? DomIn.value : DomIn.placeholder;
   const html = converter.makeHtml(md);
   DomOut.innerHTML = html;
 }
 
 DomIn.addEventListener("input", convert);
-
+convert();
 // document.querySelector(".card.submit form").onsubmit = (event) => {
 //   event.preventDefault();
 //   convert();
