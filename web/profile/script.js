7ae8c1e50c989b810c8f898166c83fb56bfefd28
10pancakes <62257032+10pancakes@users.noreply.github.com>
2022-11-02 12:24:20 +0000

    Logout button on profile page functionality added, popup for upload photo button started

--- web/profile/script.js
+++ web/profile/script.js
@@ -1,3 +1,15 @@
+<<<<<<< Updated upstream
 import { navbar } from "../nav.js";
 
 navbar();
+=======
+$(function() {
+    $( "#dialog-upload-photo" ).dialog({
+      autoOpen: false
+    });
+
+    $( ".card-upload-button" ).click(function() {
+      $( "#dialog-upload-photo" ).dialog( "open" );
+    });
+  });
+>>>>>>> Stashed changes
