03a2e83c4fe1b0605548ea74d4733d3c3ae097b3
10pancakes <62257032+10pancakes@users.noreply.github.com>
2022-11-02 16:33:57 +0000

    File upload popup completed

--- web/profile/script.js
+++ web/profile/script.js
@@ -1,12 +1,3 @@
 import { navbar } from "../nav.js";
 
 navbar();
-$(function() {
-    $( "#dialog-upload-photo" ).dialog({
-      autoOpen: false
-    });
-
-    $( ".card-upload-button" ).click(function() {
-      $( "#dialog-upload-photo" ).dialog( "open" );
-    });
-  });
