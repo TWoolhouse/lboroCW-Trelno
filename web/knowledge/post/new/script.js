16f3720bf0b04a23b4cf8e95e525d282f9fe8a10
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-11-03 11:43:46 +0000

    can create a new topic upon post creation

--- web/knowledge/post/new/script.js
+++ web/knowledge/post/new/script.js
@@ -1,5 +1,6 @@
 import { currentUser } from "../../../api/active.js";
 import * as api from "../../../api/core.js";
+import { HTMLasDOM } from "../../../nav.js";
 import * as md from "../editor.js";
 
 const converter = md.createConverter();
@@ -25,21 +26,62 @@ domPreview.querySelector(".preview-switch").addEventListener("click", () => {
   for (const switcher of switches) switcher.backward();
 });
 
-api.top
-
-document
-  .querySelector("form.post-edit")
-  .addEventListener("submit", async (event) => {
-    const form = event.target;
-
-    const topic = await api.topic(form.querySelector('[name="topic"]').value);
-
-    const post = await api.createPost(
-      topic,
-      currentUser,
-      form.querySelector('[name="title"]').value,
-      form.querySelector('[name="post"]').value
-    );
-
-    form.action = `../?id=${post.id}`;
+api.topics().then((topics) => {
+  const dropdown = domForm.querySelector('[name="topic"]');
+  dropdown.innerHTML = createDropdownTopicHTML({
+    id: "",
+    name: "Pick a Topic this Post relates to...",
   });
+  dropdown.appendChild(
+    HTMLasDOM(createDropdownTopicHTML({ id: "new", name: "Create New Topic" }))
+  );
+  for (const topic of topics.sort((a, b) => a.name.localeCompare(b.name)))
+    dropdown.appendChild(HTMLasDOM(createDropdownTopicHTML(topic)));
+});
+
+const form = document.querySelector("form.post-edit");
+form.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  const topicSelector = form.querySelector('[name="topic"]');
+  let topicId = topicSelector.value;
+  if (topicId == "new") {
+    const popup = document.querySelector("#dialog-new-topic");
+    const name = await new Promise((resolve) => {
+      popup.showModal();
+      popup
+        .querySelector('button[type="submit"]')
+        .addEventListener("click", () => {
+          popup.close();
+          resolve(popup.querySelector("input").value);
+        });
+    });
+    const topic = await api.createTopic(name);
+    topicId = topic.id;
+    topicSelector.appendChild(HTMLasDOM(createDropdownTopicHTML(topic)));
+    topicSelector.value = topicId;
+  }
+
+  const topic = await api.topic(topicId);
+  if (!topic) {
+    event.preventDefault();
+    return false;
+  }
+
+  const post = await api.createPost(
+    topic,
+    currentUser,
+    form.querySelector('[name="title"]').value,
+    form.querySelector('[name="post"]').value
+  );
+
+  // form.action = `../?id=${post.id}`;
+  window.location.href = `../?id=${post.id}`;
+});
+
+/**
+ * @param {Topic} topic
+ * @returns {String}
+ */
+function createDropdownTopicHTML(topic) {
+  return /* HTML */ `<option value="${topic.id}">${topic.name}</option>`;
+}
