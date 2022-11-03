22fdce65e551c360e166eca7ba36e7664a407b30
10pancakes <62257032+10pancakes@users.noreply.github.com>
2022-11-03 14:34:30 +0000

    Email and name of employee should now update on the user view page

--- web/profile/script.js
+++ web/profile/script.js
@@ -1,3 +1,34 @@
 import { navbar } from "../nav.js";
 
 navbar();
+
+import * as api from "../api/core.js";
+import { redirectLogin } from "../api/active.js";
+import { currentUser } from "../api/active.js";
+
+redirectLogin();
+
+
+// Read query string parameters
+const urlParams = new URLSearchParams(window.location.search);
+let userID = await api.user(urlParams.get("id"));
+if (userID == undefined){
+    userID = currentUser;
+}
+
+console.log(userID);
+
+//updates their name (on the main page)
+document.getElementById("title-card").innerHTML = userID["name"];
+
+//updating their profile picture cannot be done yet
+
+//updates their status (e.g. manager (which is the default option), team leader, nothing)
+if (userID["rank"]==1){
+    document.getElementsByClassName("heirarchy-card").innerHTML = "Team leader";
+} else if (userID["rank"]==0){
+    document.getElementsByClassName("heirarchy-card").innerHTML = "";
+} 
+
+//updates their email address
+document.getElementById("email-box").innerHTML = userID["email"];
