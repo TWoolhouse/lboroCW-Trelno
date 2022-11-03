78524cf2235da556308f34293f0e06744893ba40
10pancakes <62257032+10pancakes@users.noreply.github.com>
2022-11-03 08:58:49 +0000

    Password popup development started

--- web/profile/edit/script.js
+++ web/profile/edit/script.js
@@ -2,7 +2,7 @@ import { navbar } from "/nav.js";
 navbar();
 
 
-
+//Javascript for uploading photo window
 const updateButton = document.getElementById('card-upload-button');
 const favDialog = document.getElementById('favDialog');
 const outputBox = document.querySelector('output');
@@ -32,3 +32,35 @@ favDialog.addEventListener('close', () => {
 //will need to add code to acctually change the profile picture here
 });
 
+
+
+
+
+//Javascript for changing your password window
+const updateButtonPassword = document.getElementById('change-password-btn');
+const passwordDialog = document.getElementById('passwordDialog');
+const outputBoxPassword = document.querySelector('output');
+const confirmBtnPassword = passwordDialog.querySelector('#confirmBtnPassword');
+
+// If a browser doesn't support the dialog, then hide the
+// dialog contents by default.
+if (typeof passwordDialog.showModal !== 'function') {
+  passwordDialog.hidden = true;
+  /* a fallback script to allow this dialog/form to function
+     for legacy browsers that do not support <dialog>
+     could be provided here.
+  */
+}
+// "Update details" button opens the <dialog> modally
+updateButtonPassword.addEventListener('click', () => {
+  if (typeof passwordDialog.showModal === "function") {
+    passwordDialog.showModal();
+  } else {
+    outputBoxPassword.value = "Sorry, the <dialog> API is not supported by this browser.";
+  }
+});
+
+// "Confirm" button of form triggers "close" on dialog because of [method="dialog"]
+passwordDialog.addEventListener('close', () => {
+//will need to add code to acctually change the profile picture here
+});
