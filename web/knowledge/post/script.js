61b563fb61635345a5755eb964ada5e11316ae55
Thomas Woolhouse <thomasrwoolhouse@gmail.com>
2022-11-06 14:57:07 +0000

    mangers can edit all posts

--- web/knowledge/post/script.js
+++ web/knowledge/post/script.js
@@ -1,12 +1,17 @@
 import { post } from "./post.js";
 import * as md from "./base.js";
 import { currentUser } from "../../api/active.js";
+import { UserRank } from "../../api/model/user.js";
 
 const converter = md.createConverter();
 document.querySelector("#preview").innerHTML = converter.convert(post.markdown);
 
 const editDOM = document.querySelector("#post-edit");
-if (currentUser.id != post.owner.id) editDOM.classList.add("hidden");
+if (
+  currentUser.id != post.owner.id &&
+  currentUser.rank < UserRank.ProjectManager
+)
+  editDOM.classList.add("hidden");
 else
   editDOM.addEventListener("click", () => {
     window.location.href = `edit/?id=${post.id}`;
